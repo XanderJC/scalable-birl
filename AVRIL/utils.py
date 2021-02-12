1ce3f5691eb8904056ddfac9e0243a6561099462
XanderJC <alexjameschan@gmail.com>
2021-02-12 15:09:28 +0000

    init commit

---
+++ AVRIL/utils.py
@@ -0,0 +1,56 @@
+import jax.numpy as np
+from jax import grad, jit, vmap, value_and_grad
+from jax import random
+from jax.ops import index, index_add, index_update
+import jax.scipy.special as sc
+from jax.experimental import optimizers
+import jax
+import haiku as hk
+
+import dill
+from copy import deepcopy
+from tqdm import tqdm
+import numpy as onp
+import gym
+import argparse
+
+def load_data(env,num_trajs=None):
+
+    data = onp.load(f'volume/{env}/expert_trajs.npy',allow_pickle=True)
+    data_trajs   = data.reshape(1)[0]['trajs']
+    data_returns = data.reshape(1)[0]['returns']
+    data_dict    = data.reshape(1)[0]
+
+    if num_trajs is not None:
+        data_trajs = data_trajs[:num_trajs]
+
+    state_next_state = []
+    action_next_action = []
+
+    s_dim = data_trajs[0][0][0].shape[1]
+    for traj in data_trajs:
+
+        for t in range(len(traj)-1):
+            s_n_s = onp.zeros((2,s_dim))
+            s_n_s[0,:] = traj[t][0]
+            s_n_s[1,:] = traj[t+1][0]
+            state_next_state.append(s_n_s)
+        
+            a_n_a = onp.zeros((2,1))
+            a_n_a[0] = traj[t][1]
+            a_n_a[1] = traj[t+1][1]
+            action_next_action.append(a_n_a)
+        
+    state_next_state = onp.array(state_next_state)
+    state_next_state = np.array(state_next_state)
+
+    action_next_action = onp.array(action_next_action)
+    action_next_action = np.array(action_next_action)
+
+    a_dim = (action_next_action.max() + 1).astype(np.int32)
+
+    inputs = state_next_state
+    targets = action_next_action
+
+    return inputs,targets,a_dim,s_dim
+
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# scalable-birl
-Scalable Bayesian Inverse Reinforcement Learning (ICLR 2021)
+
+# [Scalable Bayesian Inverse Reinforcement Learning](https://openreview.net/forum?id=4qR3coiNaIv)
+
+### Alex J. Chan and Mihaela van der Schaar
+
+### International Conference on Learning Representations (ICLR) 2021
+
+Last Updated: 12 February 2021
+
+Code Author: Alex Chan (ajc340@cam.ac.uk)
+
+This repo contains a JAX based implementation of the Approximate Variational Reward Imitation Learning (AVRIL) algorithm. The code is ready to run on the control environments in the OpenAI Gym, with pre-run expert trajectories stored in the volume folder. 
+
+Example usage:
+
+```python
+from model import avril
+from utils import load_data
+
+# First setup the data, I have provided a helper function for dealing 
+# with the OpenAI gym control environemnts
+inputs,targets,a_dim,s_dim = load_data('CartPole-v1',num_trajs=15)
+
+# However AVRIL can handle any data appropriately formatted, that is inputs
+# that are (state,next_state) pairs and targets that are (action, next_action)
+# pairs:
+# inputs = [num_pairs x 2 x state_dimension]
+# targets = [num_pairs x 2 x action_dimension]
+
+agent = avril(inputs,targets,s_dim,a_dim,state_only=False)
+
+# You can define the reward to be state-only or state-action depending on use
+# Train for set number of iterations with desired batch-size
+agent.train(iters=5000,batch_size=32)
+
+# Now test by rolling out in the live Gym environment
+
+agent.gym_test('CartPole-v1')
+
+```
